b6ca5d1126bdc30ddba27197b3a02b8d884efa01
ubadineke <ubadinekeprince@gmail.com>
2024-10-31 12:31:41 +0100

    fixes

--- src/middlewares/formidable.ts
+++ src/middlewares/formidable.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 
 //Allow for upload of a single image only with a max size of 1MB
 export const uploadSingle = formidable({
-    maxFileSize: 1 * 1024 * 1024,
+    maxFileSize: 1 * 4096 * 4096,
     multiples: false,
 });
 
@@ -16,6 +16,6 @@ export const uploadSingle = formidable({
 // };
 
 export const uploadMultiple = formidable({
-    maxFileSize: 1 * 1024 * 1024,
+    maxFileSize: 1 * 4096 * 4096,
     multiples: true,
 });
